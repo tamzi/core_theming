25936db9f96443b57f331170b6f6eb39faf6d8da
tamzi <tamrefrank@gmail.com>
2021-10-06 23:33:50 +0300

    Add new button constructor

--- lib/ui/designsystem/components/tbtn.dart
+++ lib/ui/designsystem/components/tbtn.dart
@@ -7,6 +7,38 @@ import 'package:flutter/widgets.dart';
 import 'package:core_theming/ui/designsystem/theme.dart';
 
 
+class CTButtonDefault extends StatelessWidget {
+  final String label;
+  final VoidCallback? onPressed;
+
+  CTButtonDefault ({
+    required this.label,
+    this.onPressed,
+  });
+
+  /// We build our buttons calling the CTThemeData and the buttonBuilder
+  /// class.
+  /// This can be repeated for all the buttons we want to build
+  @override
+  Widget build(BuildContext context) {
+    final themeData = CTTheme.of(context);
+    return _NewButtonBuilder(
+      label: this.label,
+      btnPrimaryColor: themeData.deepGray,
+      btnShape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(20.0),
+      ),
+      onPressed: this.onPressed,
+    ).build();
+  }
+}
+
+
+
+
+
+
+
 
 
 
@@ -42,3 +74,18 @@ ElevatedButton build(){
 }
 }
 
+/*
+You can also increase size by wrapping the button
+with a constrained box like:
+child: ConstrainedBox(
+            constraints: BoxConstraints.tightFor(
+            width: 300, height: 200
+            ),
+            child: ElevatedButton(
+              child: Text('300 x 200'), under the buttons place
+              onPressed: () {},
+            ),
+          ),
+
+          see an implementation in the main.dart
+*/
